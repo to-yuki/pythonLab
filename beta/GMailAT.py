8d7b0ff7c2e5dbcf872f1dc200497b3d1a27caf3
to-yuki <to-yuki@jtp.co.jp>
2017-11-06 10:59:41 +0900

    commit

---
+++ beta/GMailAT.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+import gmail
+
+sendUsername = #u"from_user@gmail.com"
+sendUserPassword = #u"from_user_password"
+subject = u"件名"
+toAddr = #u"to_user@gmail.com"
+body = u"本文" 
+
+try:
+    # メールサーバに接続して、ログインとメール送信
+    print(u"メール送信開始")
+    client = gmail.GMail(sendUsername, sendUserPassword)
+    message = gmail.Message(subject=subject,to=toAddr,text=body)
+    client.send(message)
+    client.close()
+    print(u"メール送信完了!")
+except:
+    # メール送信エラー時の対処
+    try:
+        client.close()
+    except:
+        pass
+    print(u"メール送信エラーです。")
--- beta/SMTPMail.py
+++ beta/SMTPMail.py
@@ -1,42 +1,54 @@
 # -*- coding: UTF-8 -*-
 
 import smtplib
-from email.MIMEText import MIMEText
-from email.Header import Header
+from email.mime.text import MIMEText
+from email.header import Header
 from email.Utils import formatdate
 
 # メール送信パラメータ
 date = formatdate()
-encoding = "utf-8"
+encoding = u"utf-8"
 subject = u"件名"
-body = u"本文".encode('utf-8')
-from_addr = u"from_user@gmail.com"
-to_addrs = u"to_user@gmail.com"
-username = u"from_user@gmail.com"
-passwd = u"from_user_password"
-smtphost = "smtp.gmail.com"
+body = u"本文"
+from_addr = #u"from_user@gmail.com"
+to_addrs = #u"to_user@gmail.com"
+
+# メールサーバ設定情報
+username = #u"from_user@gmail.com"
+passwd = #u"from_user_password"
+smtphost = #u"smtp.gmail.com"
 smtpport = 587 
 
 # メールメッセージの組み立て
-msg = MIMEText(body, "plain", encoding)
-msg["Subject"] = Header(subject, encoding)
-msg["From"] = from_addr
-msg["To"] = to_addr
-msg["Date"] = date
+msg = MIMEText(body, u"plain", encoding)
+msg[u"Subject"] = Header(subject, encoding)
+msg[u"From"] = from_addr
+msg[u"To"] = to_addrs
+msg[u"Date"] = date
 
 # メールサーバに接続して、ログインとメール送信
 smtpcon = None
 try:
+    print(u"メール送信開始")
+    # メールサーバと接続
     smtpcon = smtplib.SMTP(smtphost, smtpport)
+    # HELOコマンド送信 
     smtpcon.ehlo()
+    # TLSセッションに切り替え
     smtpcon.starttls()
+    # HELOコマンド送信
     smtpcon.ehlo()
+    # メールサーバへ認証情報の送信
     smtpcon.login(username,passwd)
+    # メールデータの送信
     smtpcon.sendmail(from_addr,to_addrs,msg.as_string())
+    # QUITコマンドの送信(接続の切断)
     smtpcon.close()
+    print(u"メール送信完了!")
 except:
+    # メール送信エラー時の対処
     try:
         smtpcon.close()
     except:
         pass
-    print(u"メール送信エラーです。")
+    print(u"メール送信エラー!?")
