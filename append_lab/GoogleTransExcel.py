aa3503e2c6cbdd2674afc655da249ed609bf1521
to-yuki <to-yuki@jtp.co.jp>
2018-03-20 13:24:17 +0900

    commit

--- append_lab/GoogleTransExcel.py
+++ append_lab/GoogleTransExcel.py
@@ -20,27 +20,39 @@ for r in range(1,sheet.max_row+1):
 pattern = re.compile(r"TRANSLATED_TEXT='(.*?)'")
 # GoogleTranslateURL
 url = u'https://translate.google.com/?hl=ja#en/ja/'
-#行カウンタのリセット
-r=0
+# 行カウンタのリセット
+r = 0
+# 翻訳エラー時のリトライ回数
+retrycount = 3
 
 for origin in list:
-    if origin == None:
-        continue;
-    try:
-        
-        response = requests.get(url, params={'q': origin})
-        # resopnse 内にある JavaScript コードから翻訳された文字列を取得
-        transValue = pattern.search(response.text).group(1)
-        # ExcelSheetに書き込み
-        sheet.cell(row=r+1,column=2).value = transValue
-
-        print(u"\n[translate.google.com] で翻訳") 
-        print(u"英語:"), 
-        print(origin)
-        print(u"日本語:"), 
-        print(transValue)
+    if (origin == None or r == 0 ):
         r+=1
-    except:
-        print(str(r)+u"行目がエラーになりました。"),
+        continue;
+    # 翻訳失敗時は retrycount 回リトライする
+    while(retrycount != 0):
+        try:
+            response = requests.get(url, params={'q': origin}) 
+            # resopnse 内にある JavaScript コードから翻訳された文字列を取得
+            transValue = pattern.search(response.text).group(1)
+            # ExcelSheetに書き込み(list->0,excel->1)
+            # 翻訳テキストの書き込み列は column で指定
+            sheet.cell(row=r+1,column=2).value = transValue
+            print
+            print(str(r)+u"行目 [translate.google.com] で翻訳") 
+            print(u"\t英語:"),
+            print(origin)
+            print(u"\t日本語:"),
+            print(transValue)
+            retrycount = 3
+            break;
+        except:
+            print(str(r)+u"行目翻訳リトライ。")
+    else:
+        print(str(r)+u"行目:翻訳エラーのリトライ回数をオーバーしたので、スキップします。")
+    r+=1
 # ワークブックの保存
-book.save("GoogleTransExcel-OK.xlsx")
+try:
+    book.save("GoogleTransExcel-OK.xlsx")
+except:
+    print(u"Excelファイル保存エラーです。")
